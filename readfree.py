8a9c5b2747615bb36cd5a17e41d92c9517b33705
iHandle <ihandle@qq.com>
2019-04-02 13:44:25 +0800

    Update readfree.py

--- readfree.py
+++ readfree.py
@@ -34,19 +34,20 @@ if res.status_code == 200:
     confirm_url = 'https://readfree.me/accounts/profile/[此处需要修改为你的readfree用户名]/checkin/'
     re = requests.get(confirm_url, cookies=cookie)
     soup = BeautifulSoup(re.text, features="html.parser")
-    latest_time = soup.find('tr', class_='info').td.text
-
+    latest_time = soup.find('tr', class_='info').td
+    latest_score = latest_time.find_next() #最后一次签到获得的积分
+    total_score = latest_score.find_next() #最后一次签到后的总积分
 
     # 检查它的日期是不是今天
     date = time.strftime("%Y-%m-%d", time.localtime())
-    if latest_time.startswith(date):
-        message = '签到成功'
+    if latest_time.text.startswith(date):
+        message = '本次签到获得' + latest_score.text[1:] + '个积分，累计积分：' + total_score
 
     else:
-        message = 'cookie过期了'
+        message = '签到失败，可能是cookie过期了'
 
 else:
-    message = '签到失败'
+    message = '无法访问readfree.me'
 
 print(message)
 
